eac362788f61331029ce037eb58be89fa98df2a6
ItsLimaBean <24947193+ItsLimaBean@users.noreply.github.com>
2022-11-02 15:52:56 -0700

    Generated Icons client-side.

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,52 @@
+<script>
+import { browser } from "$app/environment";
+
+    if (browser) {
+        // const ns = "http://www.w3.org/2000/svg";
+
+        // const svg = document.createElementNS(ns, "svg");
+        // svg.setAttribute("viewBox", "0 0 512 560");
+        // svg.setAttribute("width", "512");
+        // svg.setAttribute("height", "512");
+
+        // const bg = document.createElementNS(ns, "path");
+        // bg.setAttribute("d", "M 128 181.500 L 128 235 256 235 L 384 235 384 181.500 L 384 128 256 128 L 128 128 128 181.500 M 148.006 301.153 C 140.657 304.054,132.891 312.002, 130.089 319.491 C 123.486 337.141, 133.386 356.277, 151.905 361.661 C 162.554 364.757, 175.053 361.138, 183.409 352.540 C 197.819 337.714, 193.296 311.843, 174.623 302.282 C 167.391 298.579, 155.777 298.086, 148.006 301.153 M 340.006 301.153 C 332.657 304.054, 324.891 312.002, 322.089 319.491 C 315.486 337.141, 325.386 356.277, 343.905 361.661 C 354.554 364.757, 367.053 361.138, 375.409 352.540 C 389.819 337.714, 385.296 311.843, 366.623 302.282 C 359.391 298.579, 347.777 298.086, 340.006 301.153");
+        // bg.setAttribute("stroke", "none");
+        // bg.setAttribute("fill", "#ffffff");
+        // bg.setAttribute("fill-rule", "evenodd");
+
+        // svg.appendChild(bg);
+
+        // const ic = document.createElementNS(ns, "path");
+        // ic.setAttribute("d", "M 225.500 43.045 C 182.991 44.547, 155.962 48.945, 133.176 58.067 C 114.428 65.572, 100.185 77.700, 93.070 92.217 C 84.962 108.760, 85.500 98.678, 85.500 234 L 85.500 354.500 88.329 361.840 C 92.069 371.545, 96.212 378.506, 101.516 384 L 105.861 388.500 106.190 410.500 C 106.494 430.889, 106.679 432.802, 108.715 436.624 C 111.431 441.722, 115.924 445.599, 120.795 447.050 C 123.068 447.727, 130.912 448.027, 141.090 447.827 C 157.480 447.504, 157.729 447.466, 161.807 444.637 C 164.382 442.851, 166.886 439.862, 168.467 436.691 C 170.781 432.047, 171 430.501, 171 418.804 L 171 406 256 406 L 341 406 341.022 418.750 C 341.039 428.914, 341.413 432.313, 342.865 435.509 C 344.820 439.812, 350.682 445.548, 354.676 447.067 C 356.026 447.580, 364.544 448, 373.605 448 C 391.703 448, 394.555 447.285, 400.211 441.327 C 405.195 436.078, 406 431.700, 406 409.848 L 406 389.947 411.567 383.223 C 414.629 379.526, 418.662 373.338, 420.530 369.472 C 427.373 355.311, 427.080 361.861, 426.743 230.242 C 426.402 97.535, 426.968 107.888, 419.191 92 C 414.253 81.912, 402.696 70.312, 391.128 63.834 C 376.932 55.884, 353.288 49.062, 329.500 46.054 C 306.764 43.179, 260.004 41.826, 225.500 43.045 M 128 181.500 L 128 235 256 235 L 384 235 384 181.500 L 384 128 256 128 L 128 128 128 181.500 M 148.006 301.153 C 140.657 304.054, 132.891 312.002, 130.089 319.491 C 123.486 337.141, 133.386 356.277, 151.905 361.661 C 162.554 364.757, 175.053 361.138, 183.409 352.540 C 197.819 337.714, 193.296 311.843, 174.623 302.282 C 167.391 298.579, 155.777 298.086, 148.006 301.153 M 340.006 301.153 C 332.657 304.054, 324.891 312.002, 322.089 319.491 C 315.486 337.141, 325.386 356.277, 343.905 361.661 C 354.554 364.757, 367.053 361.138, 375.409 352.540 C 389.819 337.714, 385.296 311.843, 366.623 302.282 C 359.391 298.579, 347.777 298.086, 340.006 301.153");
+        // ic.setAttribute("stroke", "none");
+        // ic.setAttribute("fill", "#000000");
+        // ic.setAttribute("fill-rule", "evenodd");
+
+        // svg.appendChild(ic);
+
+        // const textRoute = document.createElementNS(ns, "text");
+        // textRoute.setAttribute("x", "50%");
+        // textRoute.setAttribute("y", "225");
+        // textRoute.setAttribute("style", "font-size: 120px; text-anchor: middle; font-family: monospace; font-weight: bold;");
+        // textRoute.setAttribute("fill", "black");
+        // textRoute.innerHTML = "214";
+
+        // svg.appendChild(textRoute);
+
+        // const textVehicle = document.createElementNS(ns, "text");
+        // textVehicle.setAttribute("x", "50%");
+        // textVehicle.setAttribute("y", "530");
+        // textVehicle.setAttribute("style", "font-size: 100px; text-anchor: middle; font-family: monospace; font-weight: bold;");
+        // textVehicle.setAttribute("fill", "white");
+        // textVehicle.innerHTML = "19001-E";
+
+        // svg.appendChild(textVehicle);
+    }
+
+</script>
+
 <h1>Welcome to VanBusMap</h1>
 <a href="/map">MAP</a>
+
+<img src="https://icons.veryicon.com/png/o/object/material-design-icons-1/bus-22.png" alt="172 Bus Free Clipart Public Domain Vectors - Bus Png Icon @clipartmax.com">
